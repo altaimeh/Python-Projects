91496e2a241e89193b72bda3015a6d032be9bac8
Al-Taimee Hassan <altaimeh@umich.edu>
2022-04-05 14:13:53 -0400

    import resources

---
+++ grocery-store/resources.py
@@ -0,0 +1,170 @@
+resources_coffee = { 
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+resources_candy = {
+    "sugar": 3000,
+    "count": 2000,
+}
+
+resources_meat = {
+    "chicken": 500,
+    "beef": 500,
+    "steak": 500, 
+    "tenderloin": 500, 
+    "sirloin": 500,
+    "flank": 500,
+    "shrimp": 500,
+    "crab": 500,
+    "lobster": 500, 
+}
+
+resources_fruit = {
+    "green apples": 500,
+    "oranges": 500,
+    "bananas": 500,
+    "red apples": 500,
+    "apricots": 500,
+    "blueberries": 500,
+    "blackberries": 500,
+    "cherries": 500,
+    "cranberries": 500,
+    "gooseberries": 500,
+    "raspberries": 500,
+    "strawberries": 500,
+    "grapes": 500,
+    "peaches": 500,
+    "pinneaples": 500,
+    "pomegranates": 500,
+    "watermelons": 500
+}
+
+resources_veggies = {
+    "tomatoes": 500,
+    "cabbages": 500,
+    "asparaguses": 500,
+    "artichokes": 500,
+    "arugulas": 500,
+    "spinach": 500,
+    "beets": 500,
+    "bok choys": 500,
+    "green beans": 500,
+    "broccolis": 500,
+    "brussel sprouts": 500,
+    "carrots": 500,
+    "cauliflower": 500,
+    "celery": 500,
+    "cucumbers": 500,
+    "eggplants": 500,
+    "soybeans": 500,
+    "lettuce": 500,
+    "onions": 500,
+    "green peppers": 500,
+    "potatoes": 500
+}
+
+resources_deli_bread = {
+    "pasta": 500,
+    "ravioli angliolio": 500, 
+    "chocolate cake": 500,
+    "chicken salad": 500,
+    "egg salad": 500,
+    "tuna": 500,
+    "turkey": 500
+}
+
+resources_pasta_condiments = {
+    "white wine": 500,
+    "parmesan cheese": 500,
+    "butter": 500,
+    "thousand island sauce": 500,
+    "ketchup": 500,
+    "mustard": 500,
+    "teriyaki sauce": 500
+}
+
+resources_dry_goods = {
+    "cereal": 500,
+    "hummus": 500,
+    "peanut butter": 500,
+    "eggs": 500,
+    "frozen fruit": 500,
+    "tofu": 500,
+    "granola bars": 500,
+    "salmon": 500,
+    "chickpeas": 500,
+    "beef jerky": 500,
+    "canned fruit": 500,
+    "macaroni and cheese": 500
+}
+
+resources_baby_pet_department = {
+    "stuffed animals": 500,
+    "baby bath": 500,
+    "baby food maker": 500,
+    "breast-feeding bottle": 500,
+    "strollers": 500,
+    "diaper bags": 500,
+    "portable cribs": 500,
+    "bath towels": 500,
+    "pacifiers": 500,
+    "food bags": 500,
+    "potty trainers": 500,
+    "baby healing ointments": 500,
+    "thermometers": 500,
+    "baby toys": 500,
+    "baby wipes": 500,
+    "vitamin A pills": 500,
+    "vitamin B pills": 500,
+    "vitamin C pills": 500,
+    "vitamin D pills": 500,
+    "car seats": 500,
+    "sunscreen": 500
+}
+
+resources_paper_goods = {
+    "writing paper pad": 500,
+    "letter papers": 500,
+    "note-card set": 500,
+    "assorted postcards": 500,
+    "notecards": 500,
+    "cable management boxes": 500,
+    "sticky notes": 500,
+    "file organizers": 500
+}
+
+resources_dairy_department = {
+    "cheddar snack cheese": 500,
+    "mozzarella string cheese": 500,
+    "swiss premium cheese slices": 500,
+    "cottage cheese": 500,
+    "cream cheese": 500,
+    "peeled eggs": 500,
+    "lowfat 1% milk": 500,
+    "coconut milk": 500,
+    "almond milk": 500,
+    "greek yogurt": 500,
+    "plain yogurt": 500
+}
+
+resources_frozen_food = {
+    "chicken tikka masala": 500,
+    "mac and cheese": 500,
+    "orange chicken": 500,
+    "ravioli": 500,
+    "ravioli bowls": 500,
+    "corn enchiladas": 500,
+    "casserole bowls": 500,
+    "stuffed peppers": 500,
+    "enchiladas": 500,
+    "lasagna": 500,
+    "chicken pot pie": 500,
+    "burritos": 500,
+    "waffles": 500,
+    "egg white scramble": 500,
+    "cheese pizza": 500,
+    "salmon burgers": 500,
+    "falafel wrap": 500
+}
